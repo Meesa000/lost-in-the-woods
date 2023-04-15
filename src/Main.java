1ad6c5adbbbd7c14caabb30a74d07352d06702f8
Meesa000 <104768879+Meesa000@users.noreply.github.com>
2023-04-15 20:47:27 +0100

    Added getInvetory function and addInvetory function

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    private static String[] inventory = new String[5];
+    public static String[] inventory = new String[5];
 
     public static void main(String[] args) {
 
@@ -18,7 +19,10 @@ public class Main {
         //test - System.out.println(rooms);
 
 
-        welcomeScreen();
+
+        //welcomeScreen();
+        addInventory("Sword");
+        getInventory();
 
         
     }
@@ -30,13 +34,33 @@ public class Main {
         System.out.println("What is your name? ");
         String charName = scanner.nextLine();
         System.out.println("So your name is, " + charName + ".\n");
-        System.out.println("You are lost in a dark forest and need to find your way out before nightfall. " +
+        System.out.println(charName + "You are lost in a dark forest and need to find your way out before nightfall. " +
                 "You must navigate through the forest, overcome obstacles, and make strategic decisions to reach safety.");
 
 
     }
 
     public static String[] getInventory(){
+        for (int i = 0; i<inventory.length; i++){
+            if (inventory[i] == null || inventory[i].equals("")){
+                inventory[i] = "Empty";
+            }
+        }
+        System.out.println(Arrays.toString(inventory));;
         return inventory;
+    }
+
+    public static void addInventory(String item){
+
+        for (int i = 0; i< inventory.length; i++){
+            if (inventory[i] == null){
+                inventory[i] = item;
+                break;
+
+
+            }
+
+        }
+        System.out.println(Arrays.toString(inventory));
     }
 }
