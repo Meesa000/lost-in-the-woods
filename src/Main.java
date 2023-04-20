5f66b3b70115f3a14335154256e42cef09adad64
Meesa000 <104768879+Meesa000@users.noreply.github.com>
2023-04-20 11:31:28 +0100

    Added getInvetory function and addInvetory function

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -22,27 +20,26 @@ public class Main {
         starterWeapon[2] = "Bronze Bow";
 
 
+        // main game code starts here
         welcomeScreen();
         System.out.println("Choose your starter weapon");
         System.out.println("\nType 1 for melee, 2 for mage & 3 for ranged");
         String starterWepChoice = scanner.nextLine();
 
         //puts weapon in inventory depending on choice
-        boolean keepAsking = true;
-        while (keepAsking){
-            if (starterWepChoice.equals("1")){
+        while (true) {
+            if (starterWepChoice.equals("1")) {
                 System.out.println("You chose a " + starterWeapon[0]);
                 addInventory(starterWeapon[0]);
                 getInventory();
-                keepAsking = false;
+                break;
             }
+            else if (starterWepChoice.)
 
 
         }
 
         addInventory("Sword");
-
-
 
 
     }
@@ -51,24 +48,23 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
+
         System.out.println("What is your name? ");
         String charName = scanner.nextLine();
         System.out.println("So your name is, " + charName + "...\n");
-        System.out.println(charName + ", you are lost in a dark forest and need to find your way out before nightfall. " +
-                "You must navigate through the forest, overcome obstacles, and make strategic decisions to reach safety.");
+        timer(1000);
+        System.out.println(charName + ", you are lost in a dark forest and need to find your way out before nightfall. " + "You must navigate through the forest, overcome obstacles, and make strategic decisions to reach safety.");
 
 
     }
 
-    public static String[] getInventory() {
+    public static void getInventory() {
         for (int i = 0; i < inventory.length; i++) {
             if (inventory[i] == null || inventory[i].equals("")) {
-                inventory[i] = i + 1 + ". Empty";
+                inventory[i] = i + 1 + ". Empty]";
             }
         }
         System.out.println(Arrays.toString(inventory));
-        ;
-        return inventory;
     }
 
     public static void addInventory(String item) {
@@ -77,11 +73,14 @@ public class Main {
             if (inventory[i] == null) {
                 inventory[i] = i + 1 + ". " + item;
                 break;
-
-
             }
-
         }
+    }
 
+    public static void timer(int milliseconds) {
+        try {
+            Thread.sleep(milliseconds);
+        } catch (Exception e) {
+        }
     }
 }
